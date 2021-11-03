a3d1dc31e317784e4f359273d185591e6e93a2fa
Segi <SegiH@users.noreply.github.com>
2021-11-03 10:16:06 -0700

    Update schema

--- dbschema/WatchListItems.sql
+++ dbschema/WatchListItems.sql
@@ -1,7 +1,7 @@
 USE [WatchList]
 GO
 
-/****** Object:  Table [dbo].[WatchListItems]    Script Date: 10/23/2021 6:08:10 PM ******/
+/****** Object:  Table [dbo].[WatchListItems]    Script Date: 11/3/2021 10:15:15 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,12 +10,14 @@ GO
 
 CREATE TABLE [dbo].[WatchListItems](
 	[WatchListItemID] [int] IDENTITY(1,1) NOT NULL,
-	[WatchListItemName] [varchar](80) NOT NULL,
+	[WatchListItemName] [varchar](500) NOT NULL,
 	[WatchListTypeID] [int] NOT NULL,
-	[IMDB_URL] [varchar](500) NULL,
+	[IMDB_URL] [varchar](200) NULL,
+	[ItemNotes] [varchar](200) NULL,
  CONSTRAINT [PK_WatchListItems] PRIMARY KEY CLUSTERED 
 (
-	[WatchListItemID] ASC
+	[WatchListItemID] ASC,
+	[WatchListItemName] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
